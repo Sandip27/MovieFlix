3954ac601e22f02659d158857f76badb2de8a78e
Sandip Chowdhury <sandip27>
2022-07-15 11:21:55 +0530

    first commit

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "77ab6ff8";
---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
+import user from "../../images/user.png";
+import "./Header.scss";
+
+const Header = () => {
+  const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (term === "") {
+      return alert("please enter a search term");
+    }
+    dispatch(fetchAsyncMovies(term));
+    dispatch(fetchAsyncShows(term));
+    setTerm("");
+  };
+
+  return (
+    <div className="header">
+      <div className="logo">
+        <Link to="/">M O V I E F L I X</Link>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={term}
+            placeholder="Search Movies or Shows"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+      <div className="user-image">
+        <img src={user} alt="user" />
+        <p>Sandip</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
